59899db329ab40e777aa5486dad3b278c89155d1
Dong-min, Seo <dkfk2747@gmail.com>
2019-08-15 19:52:13 +0900

    Init Project
    큐챗의 서버사이드 코드입니다.

--- qchat/dbconn/dbconn.php
+++ qchat/dbconn/dbconn.php
@@ -1,9 +1,9 @@
 <?php
 
-	$DB_HOST="****";
-	$DB_USER="****";
-	$DB_PASSWORD="****";
-	$DB_DBNAME="****";
+	$DB_HOST="*****";
+	$DB_USER="*****";
+	$DB_PASSWORD="*****";
+	$DB_DBNAME="*****";
 
 	$DBCONN = pg_connect("host=$DB_HOST dbname=$DB_DBNAME user=$DB_USER password=$DB_PASSWORD")
     or die ("DB에 연결할 수 없습니다\n");
